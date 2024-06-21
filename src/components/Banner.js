6fbf56d3fbeaaa23e942eceb89ff4a1da48b6dff
WorapongWonglamduan <the_oo_ooo@hotmail.com>
2024-06-21 15:38:23 +0700

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Col, Container, Row } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import TrackVisibility from "react-on-screen";
 import headerImg from "../assets/img/wrp-g.jpg";
+import colorSharp from "../assets/img/color-sharp.png";
 import TextTypeWrite from "./utils/TextTypeWrite.js";
+
 const Banner = () => {
   return (
     <section className="banner" id="home">
@@ -15,22 +17,33 @@ const Banner = () => {
               <TrackVisibility>
                 {({ isVisible }) => (
                   <div
-                    className={
+                  /*  className={
                       isVisible ? "animate__animated animate__fadeIn" : ""
-                    }
+                    } */
                   >
                     <span className="tagline">Welcome to my Portfolio</span>
                     <h1>
-                      {`Hi! I'm Ao`} <TextTypeWrite toRotate={["WORAPONG"]} />
+                      {`Hi! I'm Ao`}{" "}
+                      <TextTypeWrite
+                        toRotate={["REACT WEB", "REACT NATIVE MOBILE"]}
+                      />
                     </h1>
 
                     <p>
-                      Lorem Ipsum is simply dummy text of the printing and
-                      typesetting industry. Lorem Ipsum has been the industry's
-                      standard dummy text ever since the 1500s, when an unknown
-                      printer took a galley of type and scrambled it to make a
-                      type specimen book.
+                      Name: Worapong Wonglamduan
+                      <br />
+                      NickName: Ao
+                      <br />
+                      Address: Khamphangphet, Thailand
+                      <br />
+                      Date of Birth: 07/01/1996
                     </p>
+
+                    <TextTypeWrite
+                      timeHide={1000}
+                      toRotate={["this website using react bootstrap"]}
+                    />
+
                     <button onClick={() => console.log("connect")}>
                       Let’s Connect <ArrowRightCircle size={25} />
                     </button>
@@ -43,9 +56,10 @@ const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) => (
                 <div
-                  className={`wrap-img-profile ${
-                    isVisible ? "animate__animated animate__zoomIn" : ""
-                  }`}
+                  // className={`wrap-img-profile ${
+                  //   isVisible ? "animate__animated animate__zoomIn" : ""
+                  // }`}
+                  className={`wrap-img-profile `}
                 >
                   <img src={headerImg} alt="Header Img" />
                 </div>
